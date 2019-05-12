de7b3655c093883bcf9b5e060c7ce08de8400bf6
Maciej Drobot <maciejdrobot@gmail.com>
2019-05-12 14:06:10 +0100

    Wersja z jedna petla

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -5,10 +5,23 @@ public class App
 {
     public static void main( String[] args )
     {
-        int[] tablica = new int[]{1, 2, 3, 4, 5};
+        int[] tablica = new int[]{4, 6, 2, 1, 3};
         System.out.println("Maks:" + max(tablica));
         System.out.println("Min:" + min(tablica));
 
+        int min = tablica[0];
+        int max = tablica[0];
+
+        for (int el : tablica){
+            if (el > max){
+                max = el;
+            }
+            if (el < min){
+                min = el;
+            }
+        }
+
+        System.out.println("Min: " + min + "  Maks: " + max);
 
     }
 
